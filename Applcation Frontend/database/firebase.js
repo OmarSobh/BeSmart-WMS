a8ecd888e091e39cce4c0d3c1b19921e108aa5f6
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2023-05-03 15:03:44 +0300

    create react native app

---
+++ Applcation Frontend/database/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_flI5H6TZkYoHXi01KJMNX1cwTpjVGf0",
+  authDomain: "be-smart-applaction.firebaseapp.com",
+  projectId: "be-smart-applaction",
+  storageBucket: "be-smart-applaction.appspot.com",
+  messagingSenderId: "541623273875",
+  appId: "1:541623273875:web:17d4923d52225cbb8f0066",
+  measurementId: "G-9SZQ9HYB0M"
+};
+
+// Initialize Firebase
