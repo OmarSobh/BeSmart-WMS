d372ecd8af4a11a167da9b29dd4f36979532e347
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2023-05-15 13:45:50 +0300

    Splash screen page Added

--- Applcation Frontend/FirebaseAuth/App.js
+++ Applcation Frontend/FirebaseAuth/App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import { ScreenStackHeaderCenterView } from 'react-native-screens';
+import SplashScreen from './screens/SplashScreen';
 
 
 
@@ -14,6 +16,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="SpplashScreen" component={SplashScreen}/>
         <Stack.Screen  name="Login" options={{headerShown:true}} component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
